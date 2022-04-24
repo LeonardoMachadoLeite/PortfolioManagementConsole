cc571c856484150bd40e2d148c12f1d1be351868
Leonardo Machado Leite <leomleite@hotmail.com>
2022-04-24 16:18:15 -0300

    Creation of Unit Test Project

---
+++ PortfolioManagementTests/ExcelTests/ExtractDayCostFromExcelFileTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PortfolioManagementTests
+{
+    [TestClass]
+    public class ExtractDayCostFromExcelFileTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
