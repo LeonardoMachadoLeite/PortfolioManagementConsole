c29f6a73940f9d48dd75cf4ae7afce470c469e0a
Leonardo Machado Leite <leomleite@hotmail.com>
2022-06-25 21:22:43 -0300

    Criação de classe de subscrição

---
+++ PortfolioManagementConsole/Domain/Bovespa/Subscricao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioManagementConsole.Domain.Bovespa
+{
+    internal class Subscricao
+    {
+        private readonly int idt;
+        private readonly string name;
+        private readonly string ticker;
+        private readonly double priceAquisition;
+
+        public Subscricao(int idt, string name, string ticker, double priceAquisition)
+        {
+            this.idt = idt;
+            this.name = name;
+            this.ticker = ticker;
+            this.priceAquisition = priceAquisition;
+        }
+
+        public int Idt => idt;
+
+        public string Name => name;
+
+        public string Ticker => ticker;
+
+        public double PriceAquisition => priceAquisition;
+    }
+}
---
+++ PortfolioManagementConsole/Domain/Common/MonthTransactions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioManagementConsole.Domain.Common
+{
+    internal class MonthTransactions
+    {
+        private readonly int month;
+        private readonly int year;
+
+        private readonly LinkedList<ITransaction> ordersList;
+        private readonly LinkedList<ISellResult> sellResultsList;
+
+        public MonthTransactions(int month, int year)
+        {
+            this.month = month;
+            this.year = year;
+
+            this.ordersList = new LinkedList<ITransaction>();
+            this.sellResultsList = new LinkedList<ISellResult>();
+        }
+
+        public int Month { get => month; }
+        public int Year { get => year; }
+        public List<ITransaction> OrdersList { get => ordersList.ToList(); }
+        public List<ISellResult> SellResultsList { get => sellResultsList.ToList(); }
+    }
+}
